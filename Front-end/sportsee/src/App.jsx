7224f3b267de0b0342e670922e2a5246e01b0538
tehami soumia <sou_mia_46@hotmail.fr>
2024-11-04 19:30:03 +0100

    correction

--- Front-end/sportsee/src/App.jsx
+++ Front-end/sportsee/src/App.jsx
@@ -1,23 +1,20 @@
 import Navbar from './composants/Navbar';
 import BarreLaterale from './composants/BarreLaterale';
 import NutrientInfo from './composants/NutrientInfo';
-import InfoUtilisateur from './composants/InfoUtilisateur'; // Correction de l'import
+import InfoUtilisateur from './composants/InfoUtilisateur';
 import './App.scss';
-
 function App() {
   return (
     <div className="app-container">
       <Navbar />
       <div className="main-content">
         <BarreLaterale />
-        <div className="content-area">
-          <InfoUtilisateur name="Thomas" /> {/* Ajout de la propriété name */}
-          <div className="nutrient-info-container">
-            <NutrientInfo type="Calories" value={2200} />
-            <NutrientInfo type="Protéines" value={130} />
-            <NutrientInfo type="Glucides" value={280} />
-            <NutrientInfo type="Lipides" value={70} />
-          </div>
+        <InfoUtilisateur name="Thomas" />
+        <div className="nutrient-info-container">
+          <NutrientInfo type="Calories" value={2200} />
+          <NutrientInfo type="Proteines" value={130} />
+          <NutrientInfo type="Glucides" value={280} />
+          <NutrientInfo type="Lipides" value={70} />
         </div>
       </div>
     </div>
