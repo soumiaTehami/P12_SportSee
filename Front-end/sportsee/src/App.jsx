35bdd0c154a49d4770ff2c6dd95ad4d5f99d4a09
tehami soumia <sou_mia_46@hotmail.fr>
2024-11-05 09:37:10 +0100

    modification scss app

--- Front-end/sportsee/src/App.jsx
+++ Front-end/sportsee/src/App.jsx
@@ -3,18 +3,21 @@ import BarreLaterale from './composants/BarreLaterale';
 import NutrientInfo from './composants/NutrientInfo';
 import InfoUtilisateur from './composants/InfoUtilisateur';
 import './App.scss';
+
 function App() {
   return (
     <div className="app-container">
       <Navbar />
       <div className="main-content">
         <BarreLaterale />
-        <InfoUtilisateur name="Thomas" />
-        <div className="nutrient-info-container">
-          <NutrientInfo type="Calories" value={2200} />
-          <NutrientInfo type="Proteines" value={130} />
-          <NutrientInfo type="Glucides" value={280} />
-          <NutrientInfo type="Lipides" value={70} />
+        <div>
+          <InfoUtilisateur name="Thomas" />
+          <div className="nutrient-info-container">
+            <NutrientInfo type="Calories" value={2200} />
+            <NutrientInfo type="Proteines" value={130} />
+            <NutrientInfo type="Glucides" value={280} />
+            <NutrientInfo type="Lipides" value={70} />
+          </div>
         </div>
       </div>
     </div>
