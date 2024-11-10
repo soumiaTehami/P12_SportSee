f5f69e409d2b34a9db1ab488b53ed98a7eac706f
tehami soumia <sou_mia_46@hotmail.fr>
2024-11-10 09:59:12 +0100

    page Error

--- Front-end/sportsee/src/pages/Error.jsx
+++ Front-end/sportsee/src/pages/Error.jsx
@@ -1,7 +1,12 @@
-
+import { Link } from 'react-router-dom';
+import './Error.scss';
 
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <div className="Main">
+      <h1 className="Title">404</h1>
+      <h2>Oops! La page que vous demandez n’existe pas</h2>
+      <Link to="/">Retourner sur la page d’accueil</Link>
+    </div>
+  );
 }
